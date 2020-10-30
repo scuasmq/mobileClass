82c32995774dcb8608c2dd5681a10789f3e45f8d
smqWin <smq115109113@163.com>
2020-10-30 21:58:19 +0800

    database connection

---
+++ testDBConnection/src/test/java/com/smq/mic/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smq.mic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
